8a79de4a84424764fd1aca8bf6a7ab7670f3b9a3
DriLLFreAK100 <blusky_8893@hotmail.com>
2021-01-29 00:47:40 +0800

    feat(worldmap): highlight coffee region and hover info

--- coffee-map/src/components/WorldMap/WorldMap.tsx
+++ coffee-map/src/components/WorldMap/WorldMap.tsx
@@ -1,15 +1,175 @@
 import * as d3 from 'd3';
+import coffeeDistributor2019 from '../../assets/coffee-distributor-2019.json';
 import geoJson from '../../assets/world.json';
-import React, { useEffect, useState } from 'react';
+import React, { SVGProps, useEffect, useState } from 'react';
 import './WorldMap.css';
 
+interface ICoffeeDistributor {
+  Rank: string;
+  Country: string;
+  Bags: string;
+  MetricTons: string;
+  Pounds: string;
+}
+
+interface ITasteProfile {
+  summary: string;
+}
+
 interface IMapCountry {
-  svgPath: string;
   countryName: string;
+  continent: string;
+  isCoffeeRegion: boolean;
+  regionUn: string;
+  regionWb: string;
+  subRegion: string;
+  tasteProfile: ITasteProfile;
+  svg: SVGProps<SVGPathElement>;
+}
+
+const exceptionCountryNames: { [key: string]: string } = {
+  'Tanzania': 'United Republic of Tanzania',
+  'Timor Leste': 'East Timor',
+}
+
+const defaultColor = '#ddd'
+const africaColor = '#d3a564'
+const africaColorAlt = '#ffd693';
+const americaColor = '#af855f';
+const americaColorAlt = '#e2b58d';
+const asiaColor = '#563625';
+const asiaColorAlt = '#84604d';
+
+const constructContries = (geoPathGenerator: d3.GeoPath<any, d3.GeoPermissibleObjects>) => {
+  const coffeeDistributorDict: { [key: string]: ICoffeeDistributor } =
+    coffeeDistributor2019.reduce((a, c: ICoffeeDistributor) => {
+      return {
+        ...a,
+        [exceptionCountryNames[c.Country] ?? c.Country]: c,
+      };
+    }, {});
+
+  const countries = geoJson.features.map((feature) => {
+    let svgProps: SVGProps<SVGPathElement> = {
+      d: geoPathGenerator(feature as any) || '',
+      stroke: defaultColor,
+      fill: defaultColor,
+    }
+
+    let isCoffeeRegion = false;
+
+    if (coffeeDistributorDict[feature.properties.ADMIN]) {
+      isCoffeeRegion = true;
+
+      svgProps = {
+        ...svgProps,
+        stroke: getRegionColor(feature.properties.REGION_UN),
+        fill: getRegionColor(feature.properties.REGION_UN),
+      }
+    }
+
+    return {
+      continent: feature.properties.CONTINENT,
+      countryName: feature.properties.ADMIN,
+      isCoffeeRegion,
+      regionUn: feature.properties.REGION_UN,
+      regionWb: feature.properties.REGION_WB,
+      subRegion: feature.properties.SUBREGION,
+      tasteProfile: getRegionTasteProfile(feature.properties.REGION_UN),
+      svg: svgProps,
+    } as IMapCountry;
+  });
+
+  return countries;
+}
+
+const getRegionColor = (region: string) => {
+  if (region.includes('America')) {
+    return americaColor;
+  } else if (region.includes('Africa')) {
+    return africaColor;
+  } else if (region.includes('Asia')) {
+    return asiaColor;
+  }
+  return defaultColor;
+};
+
+const getRegionHoverColor = (region: string) => {
+  if (region.includes('America')) {
+    return americaColorAlt;
+  } else if (region.includes('Africa')) {
+    return africaColorAlt;
+  } else if (region.includes('Asia')) {
+    return asiaColorAlt;
+  }
+  return defaultColor;
+};
+
+const getRegionTasteProfile = (region: string): ITasteProfile | null => {
+  if (region.includes('America')) {
+    return {
+      summary: 'Nutty, Chocolate'
+    };
+  } else if (region.includes('Africa')) {
+    return {
+      summary: 'Fruity, Floral'
+    };
+  } else if (region.includes('Asia')) {
+    return {
+      summary: 'Earthy, Spice, Dark Chocolate'
+    };
+  }
+  return null;
+};
+
+const isMatchCoffeeRegion = (source: IMapCountry, target: IMapCountry) => {
+  return source.isCoffeeRegion
+    && target.isCoffeeRegion
+    && source.regionUn === target.regionUn;
 }
 
 const WorldMap = () => {
   const [mapCountries, setMapCountries] = useState<IMapCountry[]>([]);
+
+  const handleMouseOverCountry = (country: IMapCountry) => {
+    const updatedCountries = mapCountries
+      .map(m => {
+        if (isMatchCoffeeRegion(m, country)) {
+          return {
+            ...m,
+            svg: {
+              ...m.svg,
+              stroke: getRegionHoverColor(country.regionUn),
+              fill: getRegionHoverColor(country.regionUn),
+            }
+          }
+        }
+
+        return m;
+      });
+
+    setMapCountries(updatedCountries)
+  };
+
+  const handleMouseLeaveCountry = (country: IMapCountry) => {
+    const updatedCountries = mapCountries
+      .map(m => {
+        if (isMatchCoffeeRegion(m, country)) {
+          return {
+            ...m,
+            svg: {
+              ...m.svg,
+              stroke: getRegionColor(country.regionUn),
+              fill: getRegionColor(country.regionUn),
+            }
+          }
+        }
+
+        return m;
+      });
+
+    setMapCountries(updatedCountries)
+  };
 
   useEffect(() => {
     const projection = d3
@@ -17,15 +177,9 @@ const WorldMap = () => {
       .fitSize([800, 400], geoJson as any);
 
     const geoPathGenerator = d3.geoPath().projection(projection);
+    const countries = constructContries(geoPathGenerator);
 
-    const country = geoJson.features.map((feature) => {
-      return {
-        svgPath: geoPathGenerator(feature as any),
-        countryName: feature.properties.ADMIN,
-      } as IMapCountry;
-    });
-
-    setMapCountries(country);
+    setMapCountries(countries);
   }, []);
 
   return (
@@ -37,11 +191,18 @@ const WorldMap = () => {
       >
         {mapCountries.map(country => {
           return (
-            <path className="WorldMap--svg--path"
+            <path
               id={country.countryName}
               key={country.countryName}
-              d={country.svgPath}
-            />
+              {...country.svg as any}
+              onMouseOver={() => handleMouseOverCountry(country)}
+              onMouseLeave={() => handleMouseLeaveCountry(country)}
+            >
+              {
+                country.tasteProfile ?
+                  <title>{country.tasteProfile.summary}</title> : null
+              }
+            </path>
           )
         })}
       </svg>
